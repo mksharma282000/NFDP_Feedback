7fb2f5225cf90988251f71df76332c373ab1034e
Mohit kumar <mohitdcsa@gmail.com>
2025-03-02 17:02:25 +0530

    Edit APIs

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -3,52 +3,53 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const {
-        name,
-        role,
-        foodRating,
-        arrangementRating,
-        overallRating,
-        comments,
-        deviceInfo,
-      } = req.body;
+  // if (req.method === "POST") {
+  //   try {
+  //     const {
+  //       name,
+  //       role,
+  //       foodRating,
+  //       arrangementRating,
+  //       overallRating,
+  //       comments,
+  //       deviceInfo,
+  //     } = req.body;
 
-      // Validate input
-      if (
-        !name ||
-        !role ||
-        !foodRating ||
-        !arrangementRating ||
-        !overallRating ||
-        !comments ||
-        !deviceInfo
-      ) {
-        return res.status(400).json({ error: "All fields are required" });
-      }
+  //     // Validate input
+  //     if (
+  //       !name ||
+  //       !role ||
+  //       !foodRating ||
+  //       !arrangementRating ||
+  //       !overallRating ||
+  //       !comments ||
+  //       !deviceInfo
+  //     ) {
+  //       return res.status(400).json({ error: "All fields are required" });
+  //     }
 
-      // Save feedback to database
-      const feedback = await prisma.feedback.create({
-        data: {
-          name,
-          role,
-          foodRating: Number(foodRating),
-          arrangementRating: Number(arrangementRating),
-          overallRating: Number(overallRating),
-          comments,
-          deviceInfo, // Now correctly storing device info
-        },
-      });
+  //     // Save feedback to database
+  //     const feedback = await prisma.feedback.create({
+  //       data: {
+  //         name,
+  //         role,
+  //         foodRating: Number(foodRating),
+  //         arrangementRating: Number(arrangementRating),
+  //         overallRating: Number(overallRating),
+  //         comments,
+  //         deviceInfo, // Now correctly storing device info
+  //       },
+  //     });
 
-      return res.status(200).json({ success: true, feedback });
-    } catch (error) {
-      console.error("Database Error:", error);
-      return res
-        .status(500)
-        .json({ error: "Something went wrong on the server" });
-    }
-  } else {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  //     return res.status(200).json({ success: true, feedback });
+  //   } catch (error) {
+  //     console.error("Database Error:", error);
+  //     return res
+  //       .status(500)
+  //       .json({ error: "Something went wrong on the server" });
+  //   }
+  // } else {
+  //   return res.status(405).json({ error: "Method not allowed" });
+  // }
+  res.status(200).json(req.method);
 }
