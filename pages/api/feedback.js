ad6d9d5e5fa53b1049ec33e85cc0ca96693dca65
Mohit kumar <mohitdcsa@gmail.com>
2025-03-02 17:48:36 +0530

    Edit APIs

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -3,32 +3,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Allow CORS preflight
-  }
   if (req.method === "POST") {
     try {
-      const {
-        name,
-        role,
-        foodRating,
-        arrangementRating,
-        overallRating,
-        comments,
-        deviceInfo,
-      } = req.body;
-      console.log("Working")
+      const { name, role, foodRating, arrangementRating, overallRating, comments, deviceInfo } = req.body;
 
       // Validate input
-      if (
-        !name ||
-        !role ||
-        !foodRating ||
-        !arrangementRating ||
-        !overallRating ||
-        !comments ||
-        !deviceInfo
-      ) {
+      if (!name || !role || !foodRating || !arrangementRating || !overallRating || !comments || !deviceInfo) {
         return res.status(400).json({ error: "All fields are required" });
       }
 
@@ -45,12 +25,10 @@ export default async function handler(req, res) {
         },
       });
 
-      return res.status(200).json({ success: true, feedback });
+      return res.status(200).json({ "success": true, ...feedback });
     } catch (error) {
       console.error("Database Error:", error);
-      return res
-        .status(500)
-        .json({ error: "Something went wrong on the server" });
+      return res.status(500).json({ error: "Something went wrong on the server" });
     }
   } else {
     return res.status(405).json({ error: "Method not allowed" });
